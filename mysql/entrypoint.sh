e7e5dddf4663b54371688d540a79430831712338
shirishakodari <kodarishirisha419@gmail.com>
2026-05-06 10:44:29 +0530

    update

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,14 +1,14 @@
-# #!/bin/bash
+#!/bin/bash
 
-# if [ -f /tmp/mysql_root_password.txt ]; then # file exits or not checks 
-#    PASSWORD=$(cat /tmp/mysql_root_password.txt) #set mysqlrootpassword set as a variable 
-#    echo "accessed root password"
-# else #if success password or not success print password file not found then exit 
+if [ -f /tmp/mysql_root_password.txt ]; then # file exits or not checks 
+   PASSWORD=$(cat /tmp/mysql_root_password.txt) #set mysqlrootpassword set as a variable 
+   echo "accessed root password"
+else #if success password or not success print password file not found then exit 
 
-#   echo "password file not found"
-#   exit 1 
-# file
-# # making it as available in shell env
-# export MYSQL_ROOT_PASSWORD=$PASSWORD
-# rm -rf /tmp/mysql_root_password.txt
-# exec /entrypoint.sh mysqld   
+  echo "password file not found"
+  exit 1 
+file
+# making it as available in shell env
+export MYSQL_ROOT_PASSWORD=$PASSWORD
+rm -rf /tmp/mysql_root_password.txt
+exec /entrypoint.sh mysqld   
